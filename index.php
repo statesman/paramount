cd454d14cc3e8470f494c4ce72d6304c03ca888a
Christian McDonald <cmcdonald@statesman.com>
2015-01-26 13:58:13 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,105 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <?php
+  $meta = array(
+    'title' => '2014 Fort Hood shooting timeline',
+    'description' => 'An Army report released Friday revealed new details about the April 2014 shooting at Fort Hood in which Spc. Ivan López killed three fellow soldiers and wounded 16 before killing himself.',
+    'thumbnail' => 'http://projects.statesman.com/news/fort-hood-timeline/assets/share.jpg',
+    'url' => 'http://projects.statesman.com/news/fort-hood-timeline/',
+    'twitter' => 'statesman'
+  );
+?>
+
+  <title>Interactive: <?php print $meta['title']; ?> | Austin American-Statesman</title>
+  <link rel="icon" type="image/png" href="//projects.statesman.com/common/favicon.ico">
+
+  <link rel="canonical" href="<?php print $meta['url']; ?>" />
+
+  <meta name="description" content="<?php print $meta['description']; ?>">
+
+  <meta property="og:title" content="<?php print $meta['title']; ?>"/>
+  <meta property="og:description" content="<?php print $meta['description']; ?>"/>
+  <meta property="og:image" content="<?php print $meta['thumbnail']; ?>"/>
+  <meta property="og:url" content="<?php print $meta['url']; ?>"/>
+
+  <meta name="twitter:card" content="summary" />
+  <meta name="twitter:site" content="@<?php print $meta['twitter']; ?>" />
+  <meta name="twitter:title" content="<?php print $meta['title']; ?>" />
+  <meta name="twitter:description" content="<?php print $meta['description']; ?>" />
+  <meta name="twitter:image" content="<?php print $meta['thumbnail']; ?>" />
+  <meta name="twitter:url" content="<?php print $meta['url']; ?>" />
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="dist/style.css">
+
+  <link href='http://fonts.googleapis.com/css?family=Lusitana:400,700' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Merriweather+Sans:400,300,300italic,400italic,700italic,700,800,800italic' rel='stylesheet' type='text/css'>
+
+  <?php /* CMG advertising and analytics */ ?>
+  <?php include "includes/advertising.inc"; ?>
+  <?php include "includes/metrics-head.inc"; ?>
+
+</head>
+<body>
+  <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="http://www.statesman.com/" target="_blank">
+          <img width="273" height="26" src="assets/logo.png">
+        </a>
+      </div>
+       <ul class="nav navbar-nav navbar-right social hidden-xs">
+          <li><a target="_blank" href="https://www.facebook.com/sharer.php?u=<?php echo urlencode($meta['url']); ?>"><i class="fa fa-facebook-square"></i></a></li>
+          <li><a target="_blank" href="https://twitter.com/intent/tweet?url=<?php echo urlencode($meta['url']); ?>&via=<?php print urlencode($meta['twitter']); ?>&text=<?php print urlencode($meta['title']); ?>"><i class="fa fa-twitter"></i></a></li>
+          <li><a target="_blank" href="https://plus.google.com/share?url=<?php echo urlencode($meta['url']); ?>"><i class="fa fa-google-plus"></i></a></li>
+        </ul>
+    </div>
+  </nav>
+
+  <div class="container">
+    <div class="row">
+      <div class="header">
+      <h4>label</h4>
+      <h1>First headline</h1>
+      <h2>Second head</h2>
+      <p><small>Interactive by Pam LeBlanc and Christian McDonald, Austin American-Statesman. <a href="pages/photo-credits.php">Photo credits</a></small></p>
+      <p>Description of your project <span style="font-weight:bold"><a href="#">and a link within it</a></span>.</p>
+      </div>
+
+
+
+    </div>
+
+    <div class="row">
+      <div class="col-lg-7 col-md-7">
+        <div id="thenAndNow">            
+          <div><img src="assets/1942-paramount-congress.jpg"  class="img-responsive" alt="slide 1" /></div>
+          <div><img src="assets/1942-paramount-congress-now.jpg" class="img-responsive" alt="slide 2" /></div>
+        </div>
+        <p class="caption clearfix"><em class="pull-right credit">Credit</em></p>
+      </div>
+    </div>
+
+
+
+  </div>
+
+    <?php /* Banner ad */ ?>
+    <?php include "includes/banner-ad.inc";?>
+
+    <p id="legal" class="center-block text-center"><small>© 2015 <a href="http://www.coxmediagroup.com" target="_blank">Cox Media Group</a>. By using this website, you accept the terms of our <a href="http://www.mystatesman.com/visitor_agreement/" target="_blank">Visitor Agreement</a> and <a target="_blank" href="http://www.mystatesman.com/privacy_policy/">Privacy Policy</a>, and understand your options regarding <a target="_blank" href="http://www.mystatesman.com/privacy_policy/#ad-choices">Ad Choices</a><img src="http://media.cmgdigital.com/shared/img/photos/2012/02/29/d3/da/ad_choices_logo.png" alt="AdChoices">.</small></p>
+
+    <?php /* CMG advertising and analytics */ ?>
+    <?php include "includes/project-metrics.inc"; ?>
+    <?php include "includes/metrics.inc"; ?>
+
+    <script src="dist/scripts.js"></script>
+
+
+
+</body>
+</html>
---
+++ src/data/timeline.js
@@ -0,0 +1,78 @@
+var timelineData = [{
+  "title": "Barnett becomes Princeton police chief",
+  "date": "1/2/2005",
+  "display_date": "2005",
+  "photo_url": "",
+  "caption": "",
+  "body": "Jeff Barnett becomes the chief of Princeton, Texas, outside Dallas. "
+}, {
+  "title": "Barnett, Hurlston meet",
+  "date": "1/2/2006",
+  "display_date": "2006",
+  "photo_url": "assets/hurlston.jpg",
+  "caption": " <span class='pull-right'>Photo by Glen Hurlston</span><br />Suzanne Hurlston in 2011",
+  "body": "Jeff Barnett and Suzanne Hurlston meet for the first time when she comes to Princeton PD to report someone had her property and wouldn't return it. Both unmarried, they begin a romantic affair several months later."
+}, {
+  "title": "Glen Hurlston and Suzanne Hurlston marry",
+  "date": "1/1/2007",
+  "display_date": "2007",
+  "photo_url": "",
+  "caption": "",
+  "body": ""
+}, {
+  "title": "Kyle hires Barnett as police chief",
+  "date": "5/1/2011",
+  "display_date": "May",
+  "photo_url": "assets/barnett.jpg",
+  "caption": " <span class='pull-right'>Ashley Landis / for Statesman</span><br />Barnett, speaking at the opening of the new Kyle Police Department",
+  "body": "Jeff Barnett is hired as Kyle's police chief"
+}, {
+  "title": "Hurlston arrested on domestic violence charge",
+  "date": "1/1/2012",
+  "display_date": "Jan. 1",
+  "photo_url": "assets/hurlstonmug.jpg",
+  "caption": " <span class='pull-right'>Collin County jail photo</span><br />Hurlston's mug shot from his 2012 arrest",
+  "body": "Dr. Glen Hurlston is arrested by Princeton police on a domestic violence charge. He is later given two years of probation, but a judge throws out his probation after about seven months."
+}, {
+  "title": "Kyle receives complaint",
+  "date": "2/1/2012",
+  "display_date": "February",
+  "photo_url": "",
+  "caption": "",
+  "body": "Kyle city leaders receive complaint against police chief."
+}, {
+  "title": "Kyle finishes internal investigation",
+  "date": "2/1/2012",
+  "display_date": "February",
+  "photo_url": "assets/earp.jpg",
+  "caption": " <span class='pull-right'>Handout photo</span><br />James Earp, who conducted Kyle's investigation",
+  "body": "Assistant City Manager James Earp finishes investigation into allegations against Barnett. He acknowledges that he never contacted the complainant because he was not given the name. He says he found no wrongdoing involving Barnett."
+}, {
+  "title": "Earp suggests Texas Rangers investigation",
+  "date": "6/1/2012",
+  "display_date": "June",
+  "photo_url": "",
+  "caption": "",
+  "body": "Earp emails his boss, City Manager Lenny Lambert, to suggest that his investigation "
+}, {
+  "title": "Hurlston released from probation",
+  "date": "9/1/2013",
+  "display_date": "September",
+  "photo_url": "",
+  "caption": "",
+  "body": "Court documents are filed that Glen Hurlston should be released from probation "
+}, {
+  "title": "Hurlston files federal lawsuit",
+  "date": "1/1/2014",
+  "display_date": "January",
+  "photo_url": "assets/hurlston-lawsuit.jpg",
+  "caption": " <span class='pull-right'>Photo by Jay Janner / Statesman</span><br />Glen Hurlston's lawsuit was filed eight months after Barnett started in Kyle",
+  "body": "Glen Hurlston files a federal lawsuit against the towns of Princeton and Kyle alleging Barnett abused his power by ordering him arrested for domestic abuse."
+}, {
+  "title": "Hurlstons divorce",
+  "date": "8/1/2014",
+  "display_date": "August",
+  "photo_url": "",
+  "caption": "",
+  "body": "Divorce between Glen Hurlston and Suzanne Hurlston finalized."
+}];
